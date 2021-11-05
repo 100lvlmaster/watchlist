1dcf70dc4f57da2c51e170f70f165398b8fbc5f4
100lvlmaster <navinkodag@gmail.com>
2021-11-05 23:57:34 +0530

    remove  storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,11 @@
-import type { NextPage } from "next";
-import {
-  VStack,
-  HStack,
-  Spacer,
-  Text,
-  Input,
-  IconButton,
-  useColorMode,
-} from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
-import AddUrlButton from "../components/add_url_button";
-
+import { NextPage } from "next";
+import Container from "../layout/container";
+import { VStack } from "@chakra-ui/react";
 const Home: NextPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const onChangeHandler = () => {
-    toggleColorMode();
-  };
   return (
-    <VStack align="stretch" w="full" minH="100vh">
-      <HStack w="full" px="10" py="5">
-        <Text fontWeight="600">Watchlist.</Text>
-        <Spacer />
-        <Input placeholder="Search" w="2xl" />
-        <AddUrlButton></AddUrlButton>
-        <Spacer />
-        <IconButton
-          onClick={onChangeHandler}
-          aria-label="theme-toggle"
-          icon={colorMode == "light" ? <MoonIcon /> : <SunIcon />}
-        />
-      </HStack>
-    </VStack>
+    <Container>
+      <VStack></VStack>
+    </Container>
   );
 };
 
